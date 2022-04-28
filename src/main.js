ab8aa77229abab12d4b6934d30231b21b4c671bb
Ioannis Manias <gmanias1991@gmail.com>
2022-04-28 23:50:16 +0300

    update imports

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import router from './router'
-import store from './store'
+import router from './router/index'
+import store from './store/index'
 
 Vue.config.productionTip = false
 
