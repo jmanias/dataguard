f62239fd04f471c9236d34ae22a90965ab9633fa
Ioannis Manias <gmanias1991@gmail.com>
2022-04-29 00:50:24 +0300

    update imports

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import './plugins/axios'
+import '@/plugins/axios'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import vuetify from '@/plugins/vuetify'
 import router from '@/router'
 import store from '@/store'
 
